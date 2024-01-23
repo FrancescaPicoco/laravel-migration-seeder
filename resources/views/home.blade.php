c45d1215cb5c994bf0be94cb38ace0d745c40271
FrancescaPicoco <francesca.picoco33@gmail.com>
2024-01-23 17:57:12 +0100

    prova

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 <h1 class="text-center">TRENI:</h1>
 <div class="cardContainer d-flex flex-wrap container-sm m-auto">
-    @foreach ($trains as $train)
+    {{-- @foreach ($trains as $train)
     <div class="card">
-        <h3>{{$train->Stazione di partenza}}</h3>
-        <h3>{{$train->Stazione di arrivo}}</h3>
+        <h3>{{$film->name}}</h3>
         <div class="card-body">
-          {{-- <p class="card-text">{{$film->description}}</p> --}}
+          <p class="card-text">{{$film->description}}</p>
         </div>
     </div>
-    @endforeach
+    @endforeach --}}
+    <h3>eccoci</h3>
 </div>   
 @endsection
