fe48cdd37756e7e3fd9fd22bdf19dad34131c842
FrancescaPicoco <francesca.picoco33@gmail.com>
2024-01-23 17:31:35 +0100

    creo il moled ed il controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dati da config/data.js: {{ $home }}</h1>
-<p>This is my paragraph content.</p>
-<button class="btn btn-primary">Cliccami</button>
+<h1 class="text-center">TRENI:</h1>
+<div class="cardContainer d-flex flex-wrap container-sm m-auto">
+    @foreach ($trains as $train)
+    <div class="card">
+        <h3>{{$train->Stazione di partenza}}</h3>
+        <h3>{{$train->Stazione di arrivo}}</h3>
+        <div class="card-body">
+          {{-- <p class="card-text">{{$film->description}}</p> --}}
+        </div>
+    </div>
+    @endforeach
+</div>   
 @endsection
