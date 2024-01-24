76be6cbe0c313804b58823eedc3cee1d5217a4c1
FrancescaPicoco <francesca.picoco33@gmail.com>
2024-01-24 10:18:20 +0100

    controller ok

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Train;
 
-
+// $trains = Train::all();
 class PageController extends Controller
-{
+{    
     public function index(){
         $trains = Train::all();
+        // dd($trains);
         return view('home', compact("trains"));
-    }
+    }  
 }
